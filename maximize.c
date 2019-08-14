a8bad36e2d92715e4a797e689a0bd07cd46d850d
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-08-14 08:28:30 -0600

    max now works in floating mode

--- maximize.c
+++ maximize.c
@@ -8,7 +8,7 @@ max(const Arg *arg)
 	/* count the number of clients  */
 	for (n = 0, nbc = nexttiled(selmon->clients); nbc; nbc = nexttiled(nbc->next), n++);
 	/* if the client is not fullscreen, alone, or monocle */
-	if (!selmon->sel->isfullscreen && (n != 1 || selmon->sel->isfloating)  && !(selmon->lt[selmon->sellt]->arrange == monocle)){
+	if (!selmon->sel->isfullscreen && (n != 1 || selmon->sel->isfloating || selmon->lt[selmon->sellt]->arrange == NULL)  && !(selmon->lt[selmon->sellt]->arrange == monocle)){
 		/* if arg is 0 use monocle size if 1 use fullscreen */
 		if(selmon->showbar && (arg->i != 1)){
 			setfullscreen(selmon->sel, 2);
