dc8ebf5b558907e4571da10c594456d9502bd128
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-01-24 20:31:19 -0700

    improved fonts

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int deltamv   = 20; /* deltamvmv = the amount of pixels ea
 static const unsigned int snap      = 32; /* snap pixel */
 static const int showbar            = 1;  /* 0 means no bar */
 static const int topbar             = 1;  /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVu Sans Mono:style=Regular:size=10"};
+static const char *fonts[]          = { "DejaVu Sans Mono:style=Regular:size=10", "FontAwesome:style=Regular:size=11"};
 static const char dmenufont[]       = "DejaVu Sans Mono:style=Regular:size=10";
 
 #include "../colorschemes/dwm/current.h"
